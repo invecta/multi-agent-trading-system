36211daaabfead757ed251824c7cc2c404c1e3bb
invecta <hindaouihani@gmail.com>
2025-09-28 18:04:00 +0200

    Add all timeframes to PythonAnywhere dashboard

--- pythonanywhere_full_dashboard.py
+++ pythonanywhere_full_dashboard.py
@@ -408,9 +408,20 @@ app.layout = html.Div([
                 dcc.Dropdown(
                     id='timeframe-dropdown',
                     options=[
-                        {'label': '1 Day', 'value': '1d'},
+                        {'label': '1 Minute', 'value': '1m'},
+                        {'label': '5 Minutes', 'value': '5m'},
+                        {'label': '15 Minutes', 'value': '15m'},
+                        {'label': '30 Minutes', 'value': '30m'},
                         {'label': '1 Hour', 'value': '1h'},
+                        {'label': '2 Hours', 'value': '2h'},
                         {'label': '4 Hours', 'value': '4h'},
+                        {'label': '6 Hours', 'value': '6h'},
+                        {'label': '8 Hours', 'value': '8h'},
+                        {'label': '12 Hours', 'value': '12h'},
+                        {'label': '1 Day', 'value': '1d'},
+                        {'label': '3 Days', 'value': '3d'},
+                        {'label': '1 Week', 'value': '1wk'},
+                        {'label': '1 Month', 'value': '1mo'},
                     ],
                     value='1d',
                     className="dropdown-container"
