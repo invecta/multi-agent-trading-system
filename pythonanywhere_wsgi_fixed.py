d0184c019c24946b5ad7506ddcd0070a3c4ac662
invecta <hindaouihani@gmail.com>
2025-09-29 03:44:02 +0200

    Fix PythonAnywhere deployment - minimal dashboard without external dependencies

--- pythonanywhere_wsgi_fixed.py
+++ pythonanywhere_wsgi_fixed.py
@@ -3,4 +3,4 @@ path = '/home/hindaouihani'
 if path not in sys.path:
     sys.path.append(path)
 
-from pythonanywhere_full_dashboard import application
+from pythonanywhere_minimal_dashboard import application
