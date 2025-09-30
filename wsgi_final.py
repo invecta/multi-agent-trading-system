0c61d5ca6cb5af42ecaaedeed02f83b501f9f6b7
invecta <hindaouihani@gmail.com>
2025-09-30 07:33:32 +0200

    Update wsgi_final.py to match PythonAnywhere configuration

--- wsgi_final.py
+++ wsgi_final.py
@@ -1,24 +1,9 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""
-WSGI configuration file for PythonAnywhere deployment
-"""
-
 import sys
 import os
 
-# Add your project directory to the Python path
-path = '/home/hindaouihani/pdf_analysis'  # Updated with actual PythonAnywhere username
+# Add your project directory to the sys.path
+path = '/home/hindaouihani/'
 if path not in sys.path:
-    sys.path.append(path)
+    sys.path.insert(0, path)
 
-# Import your Flask application
 from PRODUCTION_DASHBOARD import app as application
-
-# Set environment variables for production
-os.environ['FLASK_ENV'] = 'production'
-os.environ['DEBUG'] = 'False'
-
-if __name__ == "__main__":
-    application.run()
